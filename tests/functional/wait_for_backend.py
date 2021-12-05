7d33bea44e46b7424e7527f488cf2628ee71db6e
mechnotech <mechnotech@yandex.ru>
2021-12-05 16:46:52 +0300

    Исправление 1 ревью

--- tests/functional/wait_for_backend.py
+++ tests/functional/wait_for_backend.py
@@ -1,6 +1,6 @@
 import logging
 
-import requests
+from elasticsearch.client import Elasticsearch
 from redis import Redis
 
 from settings import config
@@ -16,17 +16,17 @@ my_logger.setLevel(logging.INFO)
 def ping_redis():
     my_logger.info(f'--------------- Connecting Redis HOST={config.redis_host} ---------------------')
     connection = Redis(config.redis_host, socket_connect_timeout=1)
-    connection.ping()
+    if not connection.ping():
+        raise ValueError('Connection to Redis failed')
     my_logger.info('=================== Redis ready =====================')
 
 
 @backoff(logy=my_logger)
 def ping_es():
     my_logger.info(f'--------------- Connecting ES HOST={config.es_host} ---------------------')
-    res = requests.get(f'http://{config.es_host}:{config.es_port}')
-    if res.json()['tagline'] != 'You Know, for Search':
-        my_logger.warning('Wait for ES!!!')
-        raise Exception
+    es = Elasticsearch([f'http://{config.es_host}:{config.es_port}/'], verify_certs=False)
+    if not es.ping():
+        raise ValueError('Connection to ES failed')
     my_logger.info('++++++++++++++++ ES ready ++++++++++++++++++++')
 
 
